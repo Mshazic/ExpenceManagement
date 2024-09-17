8b93f40339bfa061f84eb73421f7af95f5adf3ea
Mshazic <30931270+Mshazic@users.noreply.github.com>
2024-09-17 11:46:31 +0200

    Files Uploaded
    Bulk commit

---
+++ Data/ExpensesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SpendSartm.Models;
+
+namespace SpendSartm.Data
+{
+	public class ExpensesDbContext : DbContext
+	{
+		public ExpensesDbContext(DbContextOptions<ExpensesDbContext> options) : base(options) 
+		{
+		}
+
+        public DbSet<Expense> Expenses { get; set; }
+
+      
+    }
+}
+
